110d02f37c7f82bcdb37e10f4007b0a03b9cb7c0
AyushT50 <ayushtambe48@gmail.com>
2026-05-20 20:19:07 +0530

    First code!

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
 console.log("Ayush")
+
+let name = "Ayush"
+let age = 18
+let isStudent = true
+let hobbies = ["coding", "reading", "gaming"]
+let address = {
+    street: "123 Main St",
+    city: "Anytown",
+    state: "CA",
+    zip: "12345"
+}
+
+console.log(name, age, isStudent, hobbies, address)
