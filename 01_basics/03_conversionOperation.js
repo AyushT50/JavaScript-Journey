b9ce5f06798fbef659bd23585e492072d2b2a9de
AyushT50 <ayushtambe48@gmail.com>
2026-05-24 11:01:49 +0530

    learned the conversion operations!

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33"
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1
+// false => 0
+// null => 0
+// undefined => NaN
+// "123" => 123
+// "123abc" => NaN
+// "123abc" => NaN
