fcc3dd9391ff9f3ccc03c94fb1d9bd30e5ba9d19
AyushT50 <ayushtambe48@gmail.com>
2026-05-24 11:38:33 +0530

    learned comparison operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2<1);
+// console.log(2>1);
+// console.log(2<=1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1); // 1 can be number or true (boolean) also 
+// console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// stickt check (===)
+
+console.log(2 === "2");
