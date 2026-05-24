85da08ea142914a1302030ad356f411721a5e4db
AyushT50 <ayushtambe48@gmail.com>
2026-05-24 10:48:38 +0530

    learning datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,13 +1,12 @@
 const name = "Ayush"
 // let name = "Ram"
 let age = 20
-var city = "India"
-
+var city = "India" // we should not use var in modern js because of scope issues
 country = "India"
-
 age = 21
 city = "USA"
 country = "USA"
+
 console.log(name);
 
 console.table([name, age, city, country]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+//  alert("Hello"); // we are using nodejs, not browser
+
+console.log(3+3)
+
+let name ="Ayush" //number
+let age = 21 //string
+let isLoggedIn = false //boolean
+let state; //undefined
+let city = null; //null (standonle value)
+
+//symbol
+// object 
+
+console.table([name, age, isLoggedIn, state, city]);
+
+console.log(typeof state);
+console.log(typeof null);
+console.log(typeof city);
