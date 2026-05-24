5d29f14be95f02b8bdd08ec6499890b9d1e5ae21
AyushT50 <ayushtambe48@gmail.com>
2026-05-24 10:07:13 +0530

    learned varibables!

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const name = "Ayush"
+// let name = "Ram"
+let age = 20
+var city = "India"
+
+country = "India"
+
+age = 21
+city = "USA"
+country = "USA"
+console.log(name);
+
+console.table([name, age, city, country]);
