551614fb99677e94f0e4f6344784c4193d1fe788
AyushT50 <ayushtambe48@gmail.com>
2026-05-26 21:37:59 +0530

    learned math and number methods

--- 01_basics/06_math_and_number.js
+++ 01_basics/06_math_and_number.js
@@ -16,3 +16,17 @@ const hundreds = 100000
 
 // +++++++++++++++ Maths +++++++++++++++
 
+// console.log(Math);
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.9));
+// console.log(Math.max(2,4,5,1));
+// console.log(Math.min(2,4,5,1));
+
+
+console.log(Math.random());
+console.log((Math.random()*10 )+ 1);
+
+const min = 10
+const max = 20
+
+console.log((Math.random() * (max - min + 1) + min));
