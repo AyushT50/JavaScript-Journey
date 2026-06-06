3c412cae88937000a3507854a8b39faeb3b38f8a
AyushT50 <ayushtambe48@gmail.com>
2026-06-06 12:58:40 +0530

    learned date and time!

---
+++ 01_basics/07_date.js
@@ -0,0 +1,18 @@
+// dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString);
+
+
+let myCreateDate1 = new Date(2026, 0, 23)
+let myCreateDate = new Date("2026-01-23")
+// console.log(myCreateDate.toString());
+// console.log(myCreateDate1.toString());
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
+console.log(myCreateDate.getTime());
