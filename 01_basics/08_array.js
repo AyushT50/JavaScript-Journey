2081b6e64bd70a34bddb310bb21552611ea306fc
AyushT50 <ayushtambe48@gmail.com>
2026-06-06 13:37:03 +0530

    learned Array!

--- 01_basics/08_array.js
+++ 01_basics/08_array.js
@@ -29,13 +29,68 @@ const newArray = myArray.join()
 
 // slice and splice
 
-console.log("A", myArray);
+// console.log("A", myArray);
 
 const myn1 = myArray.slice(1,3)
 
-console.log(myn1);
-console.log("B", myArray);
+// console.log(myn1);
+// console.log("B", myArray);
 
 const myn2 = myArray.splice(1,3)
-console.log("C", myArray);
-console.log(myn2);
+// console.log("C", myArray);
+// console.log(myn2);
+
+
+
+
+// Array Part 2
+
+const marvel_heros = ["iron man", "thor", "hulk"]
+const dc_heros = ["superman", "batman", "flash"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+
+
+// const all_new_heros = marvel_heros.concat(dc_heros)
+// console.log(all_new_heros);
+
+
+const all_new_heros = [...marvel_heros,...dc_heros]
+// console.log(all_new_heros);
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+console.log(Array.isArray("Ayush"));
+console.log(Array.from("Ayush"));
+console.log(Array.from({name: "Ayush",
+    age: 20,
+    city: "New York",
+})); 
+
+let score1 = 100
+
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
+console.log(Object.keys({name:"Ayush",
+    age: 20,
+    city: "New York",
+}));
+
+console.log(Object.values({name:"Ayush",
+    age: 20,
+    city: "New York",
+}));
+
+console.log(Object.entries({name:"Ayush",
+    age: 20,
+    city: "New York",
+}));
