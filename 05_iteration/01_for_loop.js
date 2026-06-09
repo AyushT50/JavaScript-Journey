e5357653ddc9f5fabebd5bc96c8b8b7cf48383b7
AyushT50 <ayushtambe48@gmail.com>
2026-06-10 01:32:20 +0530

    learned while and do while loop!

--- 05_iteration/01_for_loop.js
+++ 05_iteration/01_for_loop.js
@@ -49,3 +49,4 @@ for (let i = 1; i <= 20; i++) {
     console.log(`value of i is ${i}`);
 
 }
+
---
+++ 05_iteration/02_while_do-while_loop.js
@@ -0,0 +1,27 @@
+
+let i=0
+while (i <=10 ) {
+    // console.log(`value of index is ${i}`);
+    i = i+2
+    
+    
+}
+
+let myArr = [ "flash", "batman", "spuerman"]
+
+let arr = 0
+while(arr < myArr.length){
+    console.log(`heroes are ${myArr[arr]}`);
+    arr ++
+    
+}
+
+
+// do while loop 
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score ++
+}while(score <= 10)
