fd86d0bd6e87d8ec36c2376c538cb94c50ae986c
AyushT50 <ayushtambe48@gmail.com>
2026-06-10 12:37:42 +0530

    learned forin loop

--- 05_iteration/04.js
+++ 05_iteration/04.js
@@ -1,1 +1,31 @@
-const m
+const myObj={
+    js:"javascript",
+    cpp:"C++",
+    rb:"ruby"
+}
+
+// forin  loop
+
+for (const key in myObj) {  
+    // console.log(`${key} shortcut of for ${myObj[key]}`);
+    
+}
+
+const programming = ["js","rb", "py","cpp"]
+
+
+for (const key in programming) {
+//    console.log(programming[key]);
+ 
+    }
+
+const map = new Map()
+map.set('IN',"india")
+map.set('USA',"United states of Anerica")
+map.set('IN',"india")
+
+for (const key in map) {
+    console.log(key);
+    
+}
+
