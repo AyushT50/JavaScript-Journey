f138243cdb44eee16fdc2d2f2b96240e3fd08992
AyushT50 <ayushtambe48@gmail.com>
2026-06-17 11:05:10 +0530

    DOM first part complete

--- 06_dom/one.js
+++ 06_dom/one.js
@@ -19,4 +19,51 @@
 
 // ++++++++++ DOM Manipulation ++++++++++++++
 
+// tagName : return tag for element nodes 
+// innerText : return the text content of the element and its childern
+// innerHtml : return the plain text or html conten of the element
+// textContent : return the hidden content of the element 
 
+
+// let div= document.querySelector("div")
+// console.log(div);
+
+// let heading = document.querySelector("h1")
+// console.log(h1);
+
+
+// let a = document.querySelector("h2").append(" from great Youtuber")
+// let a = document.querySelector("h2")
+
+// a.innerText = a.innerText + " from greate Youtuber!"
+
+// console.dir(a.innerText);
+
+
+
+
+// let div = document.querySelectorAll("div")
+// // div[0].innerText = div[0].innerText + " hello"
+
+// div[0].innerText += " Hello"
+// console.dir(div[0].innerText)
+
+
+// div[1].innerText += " hii"
+// console.dir(div[0].innerText)
+
+
+// div[2].innerText += " hey"
+// console.dir(div[0].innerText)
+
+
+
+
+
+let div = document.querySelectorAll("div")
+
+let idx = 1
+for(divs of div){
+    divs.innerText += ` = new unique value ${idx}`
+    idx++
+}
