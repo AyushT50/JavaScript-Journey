9859e6b45f14240150d5da0a71f4e0f966115b94
AyushT50 <ayushtambe48@gmail.com>
2026-06-17 10:11:25 +0530

    DOM manipulation

---
+++ 06_dom/one.js
@@ -0,0 +1,22 @@
+// alert("Hello Ayush!")
+
+// let heading= document.getElementById("heading");
+
+//let heading= document.getElementsByClassName("heading");
+// console.dir(heading);
+
+// let myClass = document.getElementsByClassName("myClass");
+
+// console.dir(myClass);
+
+// console.dir(document.getElementsByTagName("h3"));
+
+// console.log(document.querySelector("h1")); // return first element
+// console.log(document.querySelectorAll("h1")); // return node list 
+
+// let firstEl = document.getElementById("heading");
+// console.log(firstEl);
+
+// ++++++++++ DOM Manipulation ++++++++++++++
+
+
